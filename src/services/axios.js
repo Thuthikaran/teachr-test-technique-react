5096d0c3a80830823cdce13b5b28ed201dc41c73
Thuthikaran Easvaran <thuthikaraneasvaran@gmail.com>
2025-02-26 15:47:57 +0100

    Deploy React app to Heroku

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api',  // Point to the base URL of your Symfony API
+  baseURL: 'https://stack-shelf-b899d76c16dc.herokuapp.com/api',  // Point to the base URL of your Symfony API
   headers: {
     'Content-Type': 'application/json',
   },
