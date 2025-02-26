6f1981818bc5ed4beb03e867d1bac409c762651f
Thuthikaran Easvaran <thuthikaraneasvaran@gmail.com>
2025-02-26 16:06:01 +0100

    Fix Heroku deployment with Express

---
+++ server.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Serve static files from the "dist" folder
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
