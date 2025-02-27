0a029a4692ca58097f13210131fcbaaf24095412
Thuthikaran Easvaran <thuthikaraneasvaran@gmail.com>
2025-02-27 19:25:09 +0100

    liens changé

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,12 +6,20 @@ const NavBar = () => {
       </div>
       <div className="flex items-center">
         <a
-          href="https://thuthikaran.com"
+          href="https://github.com/Thuthikaran/teachr-test-technique"
           target="_blank"
           rel="noopener noreferrer"
           className="mr-4 flex items-center font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent"
         >
-          Portfolio
+          PHP
+        </a>
+        <a
+          href="https://github.com/Thuthikaran/teachr-test-technique-react"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-4 flex items-center font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent"
+        >
+          React
         </a>
         <a
           href="https://thuthikaran.com"
@@ -19,7 +27,7 @@ const NavBar = () => {
           rel="noopener noreferrer"
           className="mr-4 flex items-center font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent"
         >
-          Github
+          Portfolio
         </a>
       </div>
     </nav>
